a41a43e43870eb0d0128c71d19921c56ff7a11c7
Swairi Gamage <swairisg29@gmail.com>
2025-09-24 08:17:46 +0530

    created the db connection

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,17 @@
-console.log("HI there");
+//oR0eXyqEOJiU4PT6
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//middleware
+app.use("/",(req, res, next) => {
+    res.send("Hello from backend");
+})
+
+mongoose.connect("mongodb+srv://greennest_dev:oR0eXyqEOJiU4PT6@cluster0.jaiu73g.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {app.listen(5001);})
+
+.catch((err) => console.log(err));
