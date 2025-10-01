e91e6a0c4a3432c8184eb6d13588dc2dfea56571
Swairi Gamage <swairisg29@gmail.com>
2025-10-01 16:12:34 +0530

    add some css colors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // <-- change here
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client"; // <-- change here
+//import './index.css';
+import "./styles/theme.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
