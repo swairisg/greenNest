a6fff7257b7178eeddbeac0770b9c011cd9eae86
Swairi Gamage <swairisg29@gmail.com>
2025-10-02 23:53:55 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // <-- change here
-//import './index.css';
+import ReactDOM from "react-dom/client";
 import "./styles/theme.css";
+import "./styles/app.css";   // make sure this file exists, or remove this line
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-// ...existing code...
 reportWebVitals();
