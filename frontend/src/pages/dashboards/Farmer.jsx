dd3821b68396a5b83b87ed615b728b646ed2d6da
Swairi Gamage <swairisg29@gmail.com>
2025-10-02 17:26:11 +0530

    here i have created the demo pages for users dashboards to check the redirection properly

---
+++ frontend/src/pages/dashboards/Farmer.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Farmer() {
+  return <div style={{maxWidth:820,margin:"40px auto"}}><h2>Farmer / Specialist Dashboard</h2></div>;
+}
---
+++ frontend/src/pages/dashboards/Finance.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+export default function Finance() {
+  return <div style={{maxWidth:820,margin:"40px auto"}}><h2>Finance Dashboard</h2></div>;
+}
+
